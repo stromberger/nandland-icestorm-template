6fb6acb979ca94057bc55ff83db7cd5fbea135bf
Alex Stromberger <a@stromberger.org>
2019-05-27 22:52:32 +0200

    Makefile now includes all verilog files in folder src

--- src/main.v
+++ src/main.v
@@ -1,16 +1,16 @@
-module Switch_To_LEDs
-    (input i_Switch_1,
-     input i_Switch_2,
-     input i_Switch_3,
-     input i_Switch_4,
-     output o_LED_1,
-     output o_LED_2,
-     output o_LED_3,
-     output o_LED_4,);
+module main
+  (input i_Clk,
+   input  i_Switch_1,
+   input  i_Switch_2,
+   input  i_Switch_3,
+   input  i_Switch_4,
+   output o_LED_1,
+   output o_LED_2,
+   output o_LED_3,
+   output o_LED_4);
 
-assign o_LED_1 = i_Switch_1;
-assign o_LED_2 = i_Switch_2;
-assign o_LED_3 = i_Switch_3;
-assign o_LED_4 = i_Switch_4;
-
+   Blinker blinker
+     (.clock(i_Clk),
+      .switch(o_LED_1));
+          
 endmodule
